8985d3e9af5f1a5d37896d964358743f6fffd287
khushbupatel05 <khushbupatel0009@gmail.com>
2025-06-03 12:16:34 +0530

    first commit for bootstrap Landing page

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import Nav from './assets/Components/Nav'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+   <Nav/>
+  )
+}
+
+export default App
---
+++ src/assets/Components/Nav.jsx
@@ -0,0 +1,11 @@
+
+
+const Nav = () => {
+  return (
+    <div>
+        <h2>helooooo</h2>
+    </div>
+  )
+}
+
+export default Nav
